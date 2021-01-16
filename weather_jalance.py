fdbc2e7d09fc7e6af92c6a6413a09c498b0d1409
Oscar <oscargarcia.costa@telefonica.com>
2021-01-16 16:22:14 +0000

    first reformat of temperature data

--- weather_jalance.py
+++ weather_jalance.py
@@ -6,7 +6,7 @@ Created on Sat Jan 16 07:16:00 2021
 """
 
 import pandas as pd
-
+import numpy as np
 
 file_name = 'Temperaturas_Jalance.xls'
 
@@ -21,7 +21,6 @@ year_data = raw_data[str(year)]
 year_data.rename(columns = {year : 'month'}, inplace = True)
 # Fill null month names
 year_data['month'] = year_data['month'].fillna(method='ffill')
-
 
 
 
@@ -47,13 +46,32 @@ def extract_rain (year_data):
     return rain_mm
 
 
+def extract_temperature (year_data):
+    temperature = year_data.iloc[0:24,0:33]
+    
+    # Rename the months with integers
+    # Assuming the order is correct
+    temperature['month'] = np.arange(temperature.shape[0])/2 + 1
+    temperature.loc[1::2,"month"] = temperature['month']-0.5
+    temperature['month'] = temperature['month'].astype(int)
+    
+    # Rename second column
+    temperature.rename(columns = {temperature.columns[1]: 'max_min'}, inplace = True)
+    
+    # Homogenise the column that says whether it is max or min temperature
+    temperature['max_min'] = temperature['max_min'].str.lower()
+    temperature.loc[temperature['max_min'].str.contains('max'), 'max_min'] = 't_max'
+    temperature.loc[temperature['max_min'].str.contains('min'), 'max_min'] = 't_min'
+    
+    # Convert values to float
+    for col in temperature.columns[2:]:
+        temperature[col] = temperature[col].astype(float)
+    
+    temperature.set_index(['month', 'max_min'], inplace = True)
+    
+    return temperature
+
+
 rain_mm = extract_rain (year_data)
 
-
-
-temperature = year_data.iloc[0:24,0:33]
-
-
-orig = temperature.month.tolist()
-new = list(range(1, 13)) + ['Annual']
-rain_mm['month'] = rain_mm['month'].replace(orig, new)
+temperature = extract_temperature (year_data)
